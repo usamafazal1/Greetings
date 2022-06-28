7b34ff57857d385f5146ab86fdc95059e1f2d455
Usama Fazal <Usama Fazal@WIN-VOL3D49H6UK>
2022-06-29 00:38:15 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuestionNo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            // Ask for input and store that input in a string variable 
+            string name = Console.ReadLine();
+            // Print the string and concatenate the variable
+            Console.WriteLine("Hello " + name + "!");
+         
+        }
+    }
+}
